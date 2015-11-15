60892948432778698b639200231c98a6af73190d
Zigisidro <zigisidro@gmail.com>
2015-11-14 22:57:58 -0500

    Login should now move to next page?
    I really don't know, please don't ask.

--- DngrZ1/app/src/main/java/com/example/vidya/dngrz1/LoginActivity.java
+++ DngrZ1/app/src/main/java/com/example/vidya/dngrz1/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.vidya.dngrz1;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -327,6 +328,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             if (success) {
                 finish();
+                Intent moveTomain = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(moveTomain);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
